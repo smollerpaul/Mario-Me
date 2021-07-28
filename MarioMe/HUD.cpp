7e4d30df10faa7eed8fc57245bb374861f9275c7
Axel Nguyen <17520247@gm.uit.edu.vn>
2021-07-28 15:58:55 +0700

    add-hud-panel

--- MarioMe/HUD.cpp
+++ MarioMe/HUD.cpp
@@ -1,5 +1,7 @@
 #include "HUD.h"
 #include "Game.h"
+#include "Sprites.h"
+#include "PlayerData.h"
 
 void HUD::Update(DWORD dt)
 {
@@ -8,4 +10,9 @@ void HUD::Update(DWORD dt)
 void HUD::Render()
 {
 	CGame::GetInstance()->GetDirect3DDevice()->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 0, 0), 1.0f, 0);
+
+	CSprite* panel = CSprites::GetInstance()->Get("spr-hud-0");
+	panel->Draw(panel->width / 2, panel->height / 2, 1);
+
+
 }
